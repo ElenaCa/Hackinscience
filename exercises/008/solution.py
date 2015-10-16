57808ab25001685846ff5db664733da77b5e7794
Elena Calamand <elena.calamand@ri-paris.org>
2015-10-16 15:47:41 +0200

    test

--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -1,3 +1,3 @@
-list = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ', '!']
+list = ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", " ", "!"]
 for orange in list:
     print(orange)
