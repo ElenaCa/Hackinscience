8e1dae49c424a47298ad04e16b00ae62dc7a0bb3
Elena Calamand <elena.calamand@ri-paris.org>
2015-11-06 16:25:09 +0100

    test

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -1,4 +1,4 @@
 import operator
 
-print(operator.truediv(245850922,78256779))
-print(operator.truediv((1 + operator.pow(5,0.5)),2))
+print(operator.truediv(245850922, 78256779))
+print(operator.truediv((1 + operator.pow(5, 0.5)), 2))
