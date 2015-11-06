34c22ee18a4826cc3db98793fbd82847093d81c1
Elena Calamand <elena.calamand@ri-paris.org>
2015-11-06 15:19:29 +0100

    test

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,6 +1,6 @@
 import sys
 
-if len (sys.argv) > 1:
-    print (sys.argv[1])
+if len(sys.argv) > 1:
+    print(sys.argv[1])
 else:
-    sys.exit ("usage: python3 solution.py PARAM")
+    sys.exit("usage: python3 solution.py PARAM")
