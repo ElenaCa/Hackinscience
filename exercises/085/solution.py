2e58f849a3eefa8228dcf8dd0ba2ed844a7190e3
Elena Calamand <elena.calamand@ri-paris.org>
2015-12-04 16:43:49 +0100

    test

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -2,9 +2,11 @@ def sort_a_list(l):
     for i in l:
         return sorted(l, reverse=True)
 
+
 def sort_by_mark(my_class):
     for i in my_class:
         return sorted(my_class, reverse=True)
+
 
 import operator
 def sort_by_name(my_class):
